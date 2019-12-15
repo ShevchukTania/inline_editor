6a93d7a11a1130a9f2523be04b5c473b31027740
Shevchuk Tania <taniabeley@ukr.net>
2019-12-16 00:40:54 +0200

    added js for autosave

--- app/assets/javascripts/inline_editor.js
+++ app/assets/javascripts/inline_editor.js
@@ -1,31 +1,47 @@
-$(document).ready(() => {
-  $(document).on('change', '.auto-update-attribute select', function() {
-    $this = $(this);
-    var info = {};
-    var data = {};
-    data[$this.data('attribute')] = $this.find('option:selected').text();
-    modelName = $this.closest('.inline-editor-container').data('model');
-    info[modelName] = data;
+document.addEventListener('DOMContentLoaded', function(){
+  document.querySelectorAll('.form select').forEach(select => {
+    select,addEventListener("change", function(){
+      debugger;
+      var info = {};
+      var data = {};
+      data[event.target.data('attribute')] = event.target.querySelector('option:selected').text();
+      modelName = event.target.querySelectorAll.closest('.inline-editor-container').data('model');
+      info[modelName] = data;
+    })
+  }),
 
-    $.ajax({
-      type: "PATCH",
-      url: $this.parents('.auto-update-attribute').data('url'),
-      dataType: "script",
-      data: info
-    });
-  });
+  //   $this = $(this);
 
-  $(document).on('click', '.clickable-item', function() {
-    $this = $(this);
-    var container = $this.closest('.inline-editor-container');
-    container.find('.clickable-item, .auto-update-attribute').toggleClass('d-none');
-    container.find('.auto-update-attribute').toggleClass('active');
+  //
+  //   $.ajax({
+  //     type: "PATCH",
+  //     url: $this.parents('.auto-update-attribute').data('url'),
+  //     dataType: "script",
+  //     data: info
+  //   });
+  // });
 
-    $('body').on('click', function(event) {
-      if (!$(event.target).closest('.auto-update-attribute.active').length) {
-        $('.auto-update-attribute.active').removeClass('active').addClass('d-none')
-        .siblings('.clickable-item').removeClass('d-none');
-      };
+
+  document.querySelectorAll('.clickable-item').forEach(item => {
+    item.addEventListener("click", function(){
+      const container = event.target.closest('.inline-editor-container');
+      container.querySelectorAll('.clickable-item, .form').forEach(item => {
+        item.classList.toggle('display-none');
+      })
+
+      container.querySelector('.form').classList.toggle('active');
+    })
+
+    document.querySelector('body').addEventListener('click', function(){
+      const activeForm = document.querySelector('.form.active');
+      const container = event.target.closest('.inline-editor-container')
+
+      if (activeForm && !(event.target.closest('.inline-editor-container'))) {
+        console.log('ddd');
+        activeForm.classList.remove('active');
+        activeForm.classList.add('display-none');
+        activeForm.previousElementSibling.classList.remove('display-none');
+      }
     });
   });
 });
--- lib/inline_editor.rb
+++ lib/inline_editor.rb
@@ -2,9 +2,9 @@ require "inline_editor/engine"
 
 module InlineEditor
   module ApplicationHelper
-    def inline_editor(as: '', text: '', collection: '', active: '', include_blank: '', save_url: '')
+    def inline_editor(as: '', text: '', collection: '', active: '', include_blank: '', save_url: '', attribute: '', model: '')
       render 'inline_editor/inline_editor', as: as, text: text, collection: collection, active: active,
-        include_blank: include_blank, save_url: save_url
+        include_blank: include_blank, save_url: save_url, attribute: attribute, model: model
     end
   end
 end
