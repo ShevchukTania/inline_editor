193b6043b0823b4d3594a098fd49c8d5bd40a63c
Shevchuk Tania <taniabeley@ukr.net>
2019-12-19 22:47:07 +0200

    WIP

--- lib/inline_editor.rb
+++ lib/inline_editor.rb
@@ -2,8 +2,8 @@ require "inline_editor/engine"
 
 module InlineEditor
   module ApplicationHelper
-    def inline_editor(as: '', text: '', collection: '', active: '', include_blank: '', save_url: '', attribute: '', model: '')
-      render 'inline_editor/inline_editor', as: as, text: text, collection: collection, active: active,
+    def inline_editor(as: '', id: '', text: '', collection: '', active: '', include_blank: '', save_url: '', attribute: '', model: '')
+      render 'inline_editor/inline_editor', as: as, id: id, text: text, collection: collection, active: active,
         include_blank: include_blank, save_url: save_url, attribute: attribute, model: model
     end
   end
